6700149efd3bddc943537e6fa36f3d5c5fbc1fa2
Frank Warnaar <f.warnaar@viriciti.com>
2017-03-21 18:45:35 +0100

    get houses nearby

--- src/server/routes/search.js
+++ src/server/routes/search.js
@@ -1,3 +1,5 @@
+'use strict';
+
 const express = require('express');
 const router = express.Router();
 const request = require('request');
@@ -16,6 +18,38 @@ router.get('/q', (req, res) => {
 			meta: results.Metadata,
 			objects: results.Objects,
 			critical: 'search'
+		});
+	});
+});
+
+router.get('/coords/', (req, res) => {
+	const queries = url.parse(req.url, true).query;
+	const city = queries.city;
+	const lat = queries.lat;
+	const long = queries.long;
+	const type = queries.typeLocation;
+
+	request.get(`${config.google.baseUrls.maps}?latlng=${lat},${long}&key=${config.google.key}`, (err, response, body) => {
+		const location = JSON.parse(body);
+		const locationComponents = location.results[0].address_components;
+
+		let street = locationComponents.filter(component => {
+			return component.types.includes('route');
+		})[0];
+		street = street.long_name;
+
+		let city = locationComponents.filter(component => {
+			return component.types.includes('locality');
+		})[0];
+		city = city.long_name;
+
+		request.get(`${config.funda.baseUrls.search}/${config.funda.key}?type=${type}&zo=/${city}/${street}/+1km&page=1&pagesize=25`, (err, response, objects) => {
+			objects = JSON.parse(objects);
+			res.render("search", {
+				meta: objects.Metadata,
+				objects: objects.Objects,
+				critical: 'search'
+			});
 		});
 	});
 });
